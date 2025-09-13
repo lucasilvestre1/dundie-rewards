7619833e2fe8604580f4e4fa64440095e3101677
Lucas <lucas.silvestre07@gmail.com>
2025-09-13 02:55:12 -0300

    partial commit with first admin funcion #1

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,4 +1,39 @@
+import argparse
+
+def load(filepath):
+    try:
+        with open(filepath) as file_:
+            for line in file_:
+                print(line)
+    except FileNotFoundError as e:
+        print(f"File not found {e}.")
+
 
 def main():
+    parser = argparse.ArgumentParser(
+        description="Dunder Mifflin Rewards CLI",
+        epilog="Enjoy and for any doubts read the documentation"
+    )
+    parser.add_argument(
+        "subcommand",
+        type=str,
+        help="The subcommand/function to run",
+        choices=("load", "view", "send"),
+        default="help",
+    )
+    parser.add_argument(
+        "filepath",
+        type=str,
+        help="File path to load",
+        default=None,
+    )
+    args = parser.parse_args()
+    try:
+        globals()[args.subcommand](args.filepath)
+    except KeyError:
+        print("Subcommand is invalid.")
     print('Executing entry point for dundie...')
 
+
+if __name__ == "__main__":
+    main()
