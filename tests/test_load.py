2251c4c321fe7dedc9e2b81b7d34a25dc282d5ad
Lucas <lucas.silvestre07@gmail.com>
2025-09-23 00:15:33 -0300

    test once more

--- tests/test_load.py
+++ tests/test_load.py
@@ -14,4 +14,5 @@ def test_load_positive_people_list_has_size_2(request):
 @pytest.mark.unit
 @pytest.mark.high
 def test_load_positive_first_person_starts_with_j(request):
+    """Test function load"""
     assert load(PEOPLE_FILE)[0].upper().startswith('J') == True
