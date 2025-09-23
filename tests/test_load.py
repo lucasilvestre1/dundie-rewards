9a38d463ca33460b4f99d4606aee1cb3550dff91
Lucas <lucas.silvestre07@gmail.com>
2025-09-22 23:49:27 -0300

    Make all tests with an unique assert

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,8 +1,17 @@
+import os
+import uuid
 import pytest
 from dundie.core import load
 from tests.constants import PEOPLE_FILE
 
 @pytest.mark.unit
-def test_load():
-    assert load(PEOPLE_FILE)[0].startswith('Jim') == True
+@pytest.mark.high
+def test_load_positive_people_list_has_size_2(request):
+    """Test function load"""
     assert len(load(PEOPLE_FILE)) == 2
+
+
+@pytest.mark.unit
+@pytest.mark.high
+def test_load_positive_first_person_starts_with_j(request):
+    assert load(PEOPLE_FILE)[0].upper().startswith('J') == True
