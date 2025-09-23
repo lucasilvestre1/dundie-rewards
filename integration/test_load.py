425fcbd7a1c428d1d63206fb3aa714cbbb899121
Lucas <lucas.silvestre07@gmail.com>
2025-09-23 01:03:59 -0300

    A correct negative test for integrations

--- integration/test_load.py
+++ integration/test_load.py
@@ -1,10 +1,24 @@
 import pytest
-from subprocess import check_output
+from subprocess import check_output, CalledProcessError
 
 @pytest.mark.integration
-def test_load():
+@pytest.mark.medium
+def test_load_positive_call_load_command():
     """tests load command"""
     result = check_output(
         ["dundie", "load", "tests/assets/people.csv"]
     ).decode("utf-8").strip().split('\n\n')
     assert len(result) == 3
+
+
+@pytest.mark.integration
+@pytest.mark.medium
+@pytest.mark.parametrize("wrong_command", ["loady", "put", "get", "start"])
+def test_load_negative_call_load_command_with_wrong_params(wrong_command):
+    """tests load command"""
+    with pytest.raises(CalledProcessError) as error:
+        result = check_output(
+            ["dundie", wrong_command, "tests/assets/people.csv"]
+        ).decode("utf-8").strip().split('\n\n')
+    
+    assert "status 2" in str(error.getrepr())
